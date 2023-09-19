e69259d6cf0ab6002fe2b3dd9c71ebc393a3afc3
WooFoo <sujanstha3220@gmail.com>
2023-09-19 10:10:41 +0545

    prisma setup successfully

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import cors from "cors";
+import { prismaClient } from "./lib/db";
 
 async function startServer() {
 	const app = express();
@@ -15,12 +16,42 @@ async function startServer() {
             hello : String! 
             say(name : String) : String
         }
+		type Mutation {
+			createUser(firstName : String! , lastName : String! ,email : String! , password :String!) : Boolean
+		}
         `,
 		resolvers: {
 			Query: {
 				hello: () => `Hey  , I am grapql server`,
 				say: (_, { name }: { name: string }) =>
 					`Hey , ${name} , How are you?`,
+			},
+			Mutation: {
+				createUser: async (
+					_,
+					{
+						firstName,
+						lastName,
+						email,
+						password,
+					}: {
+						firstName: string;
+						lastName: string;
+						email: string;
+						password: string;
+					}
+				) => {
+					await prismaClient.user.create({
+						data: {
+							firstName,
+							lastName,
+							email,
+							password,
+							salt: "random-salt",
+						},
+					});
+					return true;
+				},
 			},
 		},
 	});
